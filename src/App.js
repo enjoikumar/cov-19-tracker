c9be734df1b170436cd244d8fe6d87882012430a
enjoikumar <kumar@secondchild.nyc>
2020-09-12 15:15:50 -0400

    basic mapping of state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA',
+    'UK',
+    'India'
+  ]);
+
   return (
     <div className="app">
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">option 1</MenuItem>
-          <MenuItem value="worldwide">option 2</MenuItem>
-          <MenuItem value="worldwide">option 3</MenuItem>
-          <MenuItem value="worldwide">option 4</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
 
-      <h1>this is app</h1>
+
+
 
       {/* Header */}
       {/* Title + select input dropdown field*/}
