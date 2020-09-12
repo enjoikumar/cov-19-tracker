87bdb1e4764544c55f9b5cb4e40fa9e20de5caf0
enjoikumar <kumar@secondchild.nyc>
2020-09-12 15:58:20 -0400

    added props to infobox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
 import { FormControl, Select, MenuItem } from "@material-ui/core";
+
+import InfoBox from "./Components/InfoBox";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -8,23 +10,20 @@ function App() {
 
   const onCountryChange = (event) => {
     const countryCode = event.target.value;
-
     setCountry(countryCode);
-  }
+  };
 
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
         .then((data) => {
-          const countries = data.map((country) => (
-            {
-              name: country.country, //United States, United Kingdom
-              value: country.countryInfo.iso2 //usa, uk, fr,
-            }
-          ));
+          const countries = data.map((country) => ({
+            name: country.country, //United States, United Kingdom
+            value: country.countryInfo.iso2, //usa, uk, fr,
+          }));
           setCountries(countries);
-        })
+        });
     };
     getCountriesData();
   }, []);
@@ -36,26 +35,24 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" onChange={onCountryChange} value={country}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map(country => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
 
+      <div class="app__stats">
+        <InfoBox title="Coronavirus Cases" total={2000} cases={123} />
 
+        <InfoBox title="Recovered" total={3000} cases={124}/>
 
+        <InfoBox title="Deaths" total={4000} cases={125}/>
+      </div>
 
-      {/* Header */}
-      {/* Title + select input dropdown field*/}
-      {/* InfoBox */}
       {/* Table*/}
       {/* Graph*/}
       {/* map*/}
-
-
     </div>
   );
 }
