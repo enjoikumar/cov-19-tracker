8a2e6a44c0d18c80e012d4aa46cc5db40ee8e10f
enjoikumar <kumar@secondchild.nyc>
2020-09-12 15:02:58 -0400

    added material ui and basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">option 1</MenuItem>
+          <MenuItem value="worldwide">option 2</MenuItem>
+          <MenuItem value="worldwide">option 3</MenuItem>
+          <MenuItem value="worldwide">option 4</MenuItem>
+        </Select>
+      </FormControl>
+
+      <h1>this is app</h1>
+
+      {/* Header */}
+      {/* Title + select input dropdown field*/}
+      {/* InfoBox */}
+      {/* Table*/}
+      {/* Graph*/}
+      {/* map*/}
+
+
     </div>
   );
 }
