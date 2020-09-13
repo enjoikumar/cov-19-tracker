6eba4b4753f72577105eb36a41170b23bbcc75b2
enjoikumar <kumar@secondchild.nyc>
2020-09-13 12:05:53 -0400

    added info box styles

--- src/Components/InfoBox.js
+++ src/Components/InfoBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
+import "../styles/InfoBox.css"
+
 
 function InfoBox({title, cases, total}) {
   return (
